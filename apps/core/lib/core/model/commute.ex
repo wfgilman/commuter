02d4010cbcc39a3b7080e641a552b4661cd96a1f
Will Gilman <will@samadhi.tech>
2018-11-23 19:32:33 -0800

    Added business logic app and query route based on stations.

---
+++ apps/core/lib/core/model/commute.ex
@@ -0,0 +1,44 @@
+defmodule Core.Model.Commute do
+  use Ecto.Schema
+  import Ecto.Query
+
+  @primary_key false
+  embedded_schema do
+    field(:route_code, :string)
+    field(:route_name, :string)
+    field(:direction, :string)
+  end
+
+  @doc """
+  Get route and direction based on start and end station. Excludes transfers.
+  """
+  def get(start_station, end_station) do
+    from(t in Db.Model.Trip,
+      join: r in assoc(t, :route),
+      join: ss in subquery(trips_through_station(start_station)),
+      on: ss.trip_id == t.id,
+      join: es in subquery(trips_through_station(end_station)),
+      on: es.trip_id == t.id,
+      where: ss.sequence < es.sequence,
+      distinct: true,
+      select: %{
+        route_code: r.code,
+        route_name: r.name,
+        direction: t.direction
+      }
+    )
+    |> Db.Repo.all()
+    |> Enum.map(&struct(__MODULE__, &1))
+  end
+
+  defp trips_through_station(station) do
+    from(s in Db.Model.Schedule,
+      join: st in assoc(s, :station),
+      where: st.code == ^station,
+      select: %{
+        trip_id: s.trip_id,
+        sequence: s.sequence
+      }
+    )
+  end
+end
