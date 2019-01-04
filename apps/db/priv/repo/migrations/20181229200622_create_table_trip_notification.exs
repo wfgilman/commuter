5736253baae22ed1bcf000fa51ea520be4db4d8e
Will Gilman <will@samadhi.tech>
2019-01-04 14:18:47 -0800

    Fix backward incompatibility

--- apps/db/priv/repo/migrations/20181229200622_create_table_trip_notification.exs
+++ apps/db/priv/repo/migrations/20181229200622_create_table_trip_notification.exs
@@ -1,7 +1,7 @@
 defmodule Db.Repo.Migrations.CreateTableTripNotification do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table("trip_notification") do
       add :device_id, :string
       add :trip_id, references(:trip, on_delete: :delete_all)
@@ -13,5 +13,12 @@ defmodule Db.Repo.Migrations.CreateTableTripNotification do
     create unique_index("trip_notification", [:device_id, :trip_id, :station_id])
     create index("trip_notification", [:trip_id])
     create index("trip_notification", [:station_id])
+  end
+
+  def down do
+    drop table("trip_notification")
+    drop_if_exists index("trip_notification", [:trip_id])
+    drop_if_exists index("trip_notification", [:station_id])
+    drop_if_exists unique_index("trip_notification", [:device_id, :trip_id])
   end
 end
