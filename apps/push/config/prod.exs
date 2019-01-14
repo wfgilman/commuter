4a74a24f7f7c05f4b27f7f077705a6e32fb0e427
Will Gilman <will@samadhi.tech>
2019-01-14 07:35:38 -0800

    Modified Pigeon prod configuration to use a worker

--- apps/push/config/prod.exs
+++ apps/push/config/prod.exs
@@ -1,9 +1,6 @@
 use Mix.Config
 
-config :pigeon, :apns,
-  apns_default: %{
-    key: "${APNS_KEY}",
-    key_identifier: "D4WPZ39FN6",
-    team_id: "QQ7F8UFQ5X",
-    mode: :prod
-  }
+config :pigeon,
+  workers: [
+    {Push.Pigeon, :apns_jwt}
+  ]
---
+++ apps/push/lib/push/pigeon.ex
@@ -0,0 +1,12 @@
+defmodule Push.Pigeon do
+
+  def apns_jwt do
+    Pigeon.APNS.JWTConfig.new(
+      name: :apns_default,
+      key: System.get_env("APNS_JWT_KEY"),
+      key_identifier: System.get_env("APNS_JWT_KEY_IDENTIFIER"),
+      team_id: System.get_env("APNS_JWT_TEAM_ID"),
+      mode: :prod
+    )
+  end
+end
