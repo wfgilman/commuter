de345ae8e0b033fc8abbe358f0eb09f58dcb1c16
Will Gilman <will@samadhi.tech>
2019-05-31 17:51:31 -0700

    Added functions to identify the correct transfer station.

--- apps/core/lib/core/commute.ex
+++ apps/core/lib/core/commute.ex
@@ -46,4 +46,153 @@ defmodule Core.Commute do
       }
     )
   end
+
+  def direct_routes(orig_station, dest_station) do
+    from(r in Db.Model.Route,
+      join: rs1 in assoc(r, :route_station),
+      join: s1 in assoc(rs1, :station),
+      join: rs2 in assoc(r, :route_station),
+      join: s2 in assoc(rs2, :station),
+      where: s1.code == ^orig_station,
+      where: s2.code == ^dest_station,
+      where: rs1.sequence < rs2.sequence
+    )
+  end
+
+  def get_direct_routes(orig_station, dest_station) do
+    Db.Repo.all(direct_routes(orig_station, dest_station))
+  end
+
+  # 1. Find all routes running through origin station.
+  def routes_through_station(station) do
+    from(rs in Db.Model.RouteStation,
+      join: s in assoc(rs, :station),
+      where: s.code == ^station,
+      select: %{
+        route_id: rs.route_id,
+        sequence: rs.sequence
+      }
+    )
+  end
+
+  # 2. Find all transfer stations downstream on routes from origin station.
+  def transfer_stations_downstream(station) do
+    from(s in Db.Model.Station,
+      join: rs in Db.Model.RouteStation,
+      on: rs.station_id == s.id,
+      join: rts in subquery(routes_through_station(station)),
+      on: rts.route_id == rs.route_id,
+      left_join: t in Db.Model.Transfer,
+      on: rs.route_id == t.from_route_id and rs.station_id == t.station_id,
+      where: rs.sequence > rts.sequence,
+      where: not is_nil(t.id),
+      distinct: true,
+      select: s
+    )
+  end
+
+  # 3. Select all transfer stations that are upstream from destination station.
+  def transfer_stations_upstream(origin, destination) do
+    from(s in Db.Model.Station,
+      join: rs in Db.Model.RouteStation,
+      on: rs.station_id == s.id,
+      join: rts in subquery(routes_through_station(destination)),
+      on: rts.route_id == rs.route_id,
+      join: us in subquery(transfer_stations_downstream(origin)),
+      on: us.id == rs.station_id,
+      left_join: t in Db.Model.Transfer,
+      on: rs.route_id == t.from_route_id and rs.station_id == t.station_id,
+      # where: rs.sequence > rts.sequence,
+      where: not is_nil(t.id),
+      distinct: true,
+      select: s
+    )
+  end
+
+  # 4. Select transfer station with minimum number of stops.
+  def transfer_station_with_min_stops(origin, destination) do
+    transfer_stations = Db.Repo.all(transfer_stations_upstream(origin, destination))
+
+    stops =
+      from(rs in Db.Model.RouteStation,
+        join: s in assoc(rs, :station),
+        join: r in assoc(rs, :route),
+        preload: [station: s, route: r],
+        order_by: rs.sequence
+      )
+      |> Db.Repo.all()
+
+    transfer_stations
+    |> Enum.map(fn station ->
+      %{
+        transfer_station: station,
+        upstream_route: List.first(get_direct_routes(origin, station.code))
+      }
+    end)
+    |> Enum.map(fn %{transfer_station: station} = result ->
+      Map.put(result, :downstream_route, List.first(get_direct_routes(station.code, destination)))
+    end)
+    |> Enum.map(fn %{transfer_station: station} = result ->
+      Map.put(
+        result,
+        :upstream_stops,
+        stops_between_stations_on_same_route(
+          stops,
+          result.upstream_route,
+          origin,
+          station.code
+        )
+      )
+    end)
+    |> Enum.map(fn %{transfer_station: station} = result ->
+      Map.put(
+        result,
+        :downstream_stops,
+        stops_between_stations_on_same_route(
+          stops,
+          result.downstream_route,
+          station.code,
+          destination
+        )
+      )
+    end)
+    |> Enum.map(fn result ->
+      total_stops = result.upstream_stops + result.downstream_stops
+
+      %{
+        transfer_station: result.transfer_station,
+        total_stops: total_stops
+      }
+    end)
+    |> Enum.sort_by(& &1.total_stops)
+    |> List.first()
+    |> Map.get(:transfer_station)
+  end
+
+  # 5. Find all trips between origin station and transfer station.
+
+  def stops_between_stations_on_same_route(_stops, _route, orig, dest) when orig == dest, do: 0
+
+  def stops_between_stations_on_same_route(stops, route, origin, destination) do
+    route_stops = Enum.filter(stops, &(&1.route.code == route.code))
+    orig_seq = get_station_sequence_on_route(route_stops, origin)
+    dest_seq = Enum.find(route_stops, &(&1.station.code == destination)).sequence
+
+    route_stops
+    |> Enum.reject(fn stop ->
+      stop.sequence <= orig_seq or stop.sequence > dest_seq
+    end)
+    |> Enum.count()
+  end
+
+  def get_station_sequence_on_route(stops, station) do
+    case Enum.find(stops, &(&1.station.code == station)) do
+      nil ->
+        IO.puts("Couldn't find station #{station}")
+        nil
+
+      stop ->
+        stop.sequence
+    end
+  end
 end
