0f545c302c394d8148180f08ddb506edade9c4e9
Will Gilman <will@samadhi.tech>
2020-02-10 07:08:43 -0800

    Post permanent advisory until 2/10 changes are incorporated.

--- apps/api/lib/api_web/views/advisory_view.ex
+++ apps/api/lib/api_web/views/advisory_view.ex
@@ -2,6 +2,10 @@ defmodule ApiWeb.AdvisoryView do
   use ApiWeb, :view
 
   def render("index.json", %{data: advisory}) do
-    Map.from_struct(advisory)
+    # Map.from_struct(advisory)
+    %{
+      count: 1,
+      advisory: "Feb 10th schedule changes have not been incorporated yet. Departures may not be accurate."
+    }
   end
 end
