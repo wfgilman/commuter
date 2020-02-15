2e4310c7680fa71b0fc7ca3337f2a504187e4484
Will Gilman <will@samadhi.tech>
2020-02-14 18:25:28 -0800

    Remove Feb 10th advisor notification override.

--- apps/api/lib/api_web/views/advisory_view.ex
+++ apps/api/lib/api_web/views/advisory_view.ex
@@ -2,10 +2,6 @@ defmodule ApiWeb.AdvisoryView do
   use ApiWeb, :view
 
   def render("index.json", %{data: advisory}) do
-    # Map.from_struct(advisory)
-    %{
-      count: 1,
-      advisory: "Feb 10th schedule changes have not been incorporated yet. Departures may not be accurate."
-    }
+    Map.from_struct(advisory)
   end
 end
